e4a8c52f14e6a6b943e0184d47001eb2e25fc148
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2025-09-19 19:17:28 +0200

    it works! Update better_battery_estimator.ps1
    functional!!

--- better_battery_estimator.ps1
+++ better_battery_estimator.ps1
@@ -6,65 +6,50 @@ $max_entries = 10
 
 # Start an infinite loop to keep checking battery status
 while ($true) {
-
+	
+	# Get the current timestamp
+    $timestamp = Get-Date
+	
     # Get battery info using Windows Management Instrumentation (WMI)
     $battery = Get-WmiObject -Class Win32_Battery
-
+	
     # Extract the current battery percentage
-    $percent = $battery.EstimatedChargeRemaining
-	$run_time = $battery.EstimatedRunTime
-	Write-Host $run_time
-
-    # Get the current timestamp
-    $timestamp = Get-Date
-	# Write-Host "$timestamp$"
-
+	$runtime = $battery.EstimatedRunTime
+	$percent = $battery.EstimatedChargeRemaining
+	
     # Create a custom object to store this reading
     $entry = [PSCustomObject]@{
-        Time = $timestamp
-        Percent = $percent
+        time = $timestamp
+        runtime = $runtime
+		percent = $percent
     }
-
+	
     # Add the new entry to our log array
     $log += $entry
-
+	
     # If we have more than $maxEntries, trim the oldest ones
-    if ($log.Count -gt $maxEntries) {
-        # Keep only the last $maxEntries readings
-        $log = $log[-$maxEntries..-1]
+    if ($log.Count -gt $max_entries) {
+        # Keep only the last $max_entries readings
+        $log = $log[-$max_entries..-1]
     }
-
-    # If we have at least two readings, we can estimate battery drain rate
+	
+    # If we have at least two readings, we can estimate remaining battery time
     if ($log.Count -ge 2) {
-        # Get the first and last readings in the log
-        $start = $log[0]
-        $end = $log[-1]
-
-        # Calculate how much battery was used
-        $deltaPercent = $start.Percent - $end.Percent
-
-        # Calculate how much time passed (in minutes)
-        $deltaTime = ($end.Time - $start.Time).TotalMinutes
-
-        # If battery was actually used (i.e., not charging or idle)
-        if ($deltaPercent -gt 0) {
-            # Calculate the average drain rate: percent per minute
-            $rate = $deltaPercent / $deltaTime
-
-            # Estimate how many minutes are left at this rate
-            $estimatedMinutesLeft = $end.Percent / $rate
-
-            # Print the result
-            Write-Host "Battery: $percent% | Estimated time left: $([math]::Round($estimatedMinutesLeft)) minutes"
-        } else {
-            # If battery didn't drain, we can't estimate yet
-            Write-Host "Battery: $percent% | Estimating..."
-        }
+		$runtime_sum = 0
+		for ($i=0; $i -lt $log.Count; $i++) {
+			$this_entry = $log[$i]
+			$runtime_sum = $runtime_sum + $this_entry.runtime
+		}
+		
+		$average_runtime = [math]::Round(($runtime_sum / $log.Count), 2)
+		
+		Write-Host "Battery Charge Remaining: " $log[-1].percent
+		Write-Host "Battery Time Remaining: " $average_runtime
     } else {
         # Not enough data yet to estimate
         Write-Host "Battery: $percent% | Gathering data..."
     }
-
+	
     # Wait 2 seconds before checking again
     Start-Sleep -Seconds 2
 }
